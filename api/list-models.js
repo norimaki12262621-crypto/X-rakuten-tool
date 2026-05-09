eaec0c75d1816970dfdb12bbb096483f5a41764f
norimaki12262621-crypto <norimaki12262621@gmail.com>
2026-05-09 11:48:58 +0900

    Remove temp list-models endpoint

--- api/list-models.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  const key = process.env.GEMINI_API_KEY;
-  if (!key) return res.status(500).json({ error: 'GEMINI_API_KEY not set' });
-  const r = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${key}`);
-  const data = await r.json();
-  return res.status(200).json(data);
-};
